fc3ec1257661dda9c25c15281ff13313e25da5b4
Neriya Rosner <neri.coder@gmail.com>
2023-06-19 12:21:31 +0300

    add commit exclusion (unfinished)

--- releaser.js
+++ releaser.js
@@ -1,47 +1,86 @@
 #!/usr/bin/env node
-const { execSync } = require("child_process");
+const { execSync } = require('child_process');
 const args = process.argv.slice(2);
+const environment = args[0];
+const excludedCommitHash = args[1];
+
+if (['test', 'prod'].indexOf(environment) === -1) {
+    console.log('Usage: releaser.js [test|prod] [optional: commitHash to revert]');
+    return;
+}
+
+release(environment, excludedCommitHash);
+
 
 function getLastCommitHash(length = 6) {
-  return execSync("git rev-parse HEAD").toString().trim().slice(0, length+1);
+    return execSync('git rev-parse HEAD').toString().trim().slice(0, length + 1);
 }
 
 function getBranchName() {
-  return execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
+    return execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 }
 
 function createAndPushRelease(releaseName) {
-  if (checkIfTagExists(releaseName)) {
-    console.log(`Release ${releaseName} already exists`);
-    return;
-  }
-  console.log(`Releasing ${releaseName} on ${getBranchName()}`);
-  execSync(`git tag ${releaseName}`)
-  execSync(`git push origin ${releaseName}`)
+    if (checkIfTagExists(releaseName)) {
+        console.log(`Release ${releaseName} already exists`);
+        return;
+    }
+    console.log(`Releasing ${releaseName} on ${getBranchName()}`);
+    execSync(`git tag ${releaseName}`);
+    execSync(`git push origin ${releaseName}`);
 }
 
-// checkIfTagExists without stdout
 function checkIfTagExists(tagName) {
-  try {
-    execSync(`git rev-parse --verify --quiet ${tagName}`);
-    return true;
-  } catch (error) {
-    return false;
-  }
+    try {
+        const res = execSync(`git rev-parse --verify --quiet ${tagName}`);
+        return res.toString().trim().length > 0;
+    } catch (error) {
+        return false;
+    }
 }
 
-function release() {
-  const environment = args[0];
-
-  if (["test", "prod"].indexOf(environment) === -1) {
-    console.log("Usage: releaser.js [test|prod]");
-    return;
-  }
-
-  const commitHash = getLastCommitHash();
-  const releaseName = `${environment}-${commitHash}`;
-
-  createAndPushRelease(releaseName);
+function checkIfCommitExists(commitHash) {
+    try {
+        const res = execSync(`git rev-parse --verify --quiet ${commitHash}`);
+        return res.toString().trim() === commitHash;
+    } catch (error) {
+        return false;
+    }
 }
 
-release();
+// function revertCommit(commitHash) {
+//     try {
+//         if (checkIfCommitExists(commitHash)) {
+//             execSync(`git revert ${commitHash}`);
+//             console.log(`Reverted commit ${commitHash}`);
+//         }
+//     } catch (error) {
+//
+//     }
+// }
+
+// function cherryPickCommit(commitHash) {
+//     try {
+//         if (checkIfCommitExists(commitHash)) {
+//             execSync(`git cherry-pick --all ${commitHash}`);
+//             console.log(`Cherry picked commit ${commitHash}`);
+//         }
+//     } catch (error) {
+//
+//     }
+// }
+
+function release(environment, excludedCommitHash) {
+    // if (excludedCommitHash && environment === 'prod') {
+    //     revertCommit(excludedCommitHash);
+    // }
+
+    const commitHash = getLastCommitHash();
+    const releaseName = `${environment}-${commitHash}`;
+
+    createAndPushRelease(releaseName);
+
+    // if (excludedCommitHash && environment === 'prod') {
+    //     cherryPickCommit(excludedCommitHash);
+    // }
+}
