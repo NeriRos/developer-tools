0d7d891ad9659e081c6cc6ba58901252b786318d
Neriya Rosner <neri.coder@gmail.com>
2023-07-03 10:47:55 +0300

    add check for remote commit

--- releaser.js
+++ releaser.js
@@ -13,19 +13,32 @@ release(environment, excludedCommitHash);
 
 
 function getLastCommitHash(length = 6) {
-    return execSync('git rev-parse HEAD').toString().trim().slice(0, length + 1);
+    const commitHash = execSync('git rev-parse HEAD').toString().trim();
+
+    if (length === -1) {
+        return commitHash;
+    }
+
+    return commitHash.slice(0, length + 1);
 }
 
 function getBranchName() {
     return execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 }
 
-function createAndPushRelease(releaseName) {
+function createAndPushRelease(releaseName, commitHash) {
     if (checkIfTagExists(releaseName)) {
         console.log(`Release ${releaseName} already exists`);
         return;
     }
+
+    if (!checkIfCommitExistsInOrigin(commitHash)) {
+        console.log(`Commit ${commitHash} does not exist in origin`);
+        return;
+    }
+
     const branchName = getBranchName();
+
     if (releaseName.startsWith("prod-") && branchName !== 'master') {
         console.log(`Production releases can only be created from master branch. Current branch is ${branchName}`);
         return;
@@ -45,9 +58,24 @@ function checkIfTagExists(tagName) {
     }
 }
 
+function checkIfCommitExistsInOrigin(commitHash) {
+    try {
+        const res = execSync(`git branch -r --quiet --contains ${commitHash}`);
+        return res.toString().trim().indexOf("no such commit") === -1;
+    } catch (error) {
+        return false;
+    }
+}
+
+function fetchGit() {
+    execSync('git fetch');
+}
+
 function release(environment, excludedCommitHash) {
+    fetchGit();
     const commitHash = getLastCommitHash();
+    const commitHashComplete = getLastCommitHash(-1);
     const releaseName = `${environment}-${commitHash}`;
 
-    createAndPushRelease(releaseName);
+    createAndPushRelease(releaseName, commitHashComplete);
 }
